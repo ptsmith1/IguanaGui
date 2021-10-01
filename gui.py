edb7c8b3eaa50026cca5777a46f41fc67b0c368d
Philip Smith <ptsmith2808@gmail.com>
2021-10-01 17:52:42 +0100

    Updated the main window so that new tabs can be created which load images chosen by the user. I have also added a second class which creates a window allowing the user to input the locations of the key files needed to run Iguana. When the main Iguana program is more developed these paths will be used to run it.

--- gui.py
+++ gui.py
@@ -1,182 +1,232 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
-from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget, QPushButton, QTextBrowser, QHBoxLayout, QVBoxLayout, QTabWidget, QMenu, QAction, QFileDialog,\
+    QSizePolicy, QMessageBox, QScrollArea, QTextEdit, QStyle
+from PyQt5.QtCore import QSize
+from PyQt5.QtGui import QPixmap, QImage, QPalette, QIcon
 from PyQt5.Qt import Qt
-from PySide2.QtCore import (QCoreApplication, QMetaObject, QObject, QPoint,
-    QRect, QSize, QUrl, )
-from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
-    QFontDatabase, QIcon, QLinearGradient, QPalette, QPainter, QPixmap,
-    QRadialGradient, QImage)
-from PySide2.QtWidgets import *
-from PyQt5 import QtWidgets
+import sys
 
 
-class ImageLabel(QLabel):
+class MainWindow(QMainWindow):
     """
-    class label
+    This is the main class for the UI
     """
     def __init__(self):
         super().__init__()
-        # self = QLabel(self.tab)
-        self.setObjectName(u"label")
-        # self.keyPressEvent = self.keyPressEvent
 
-    def keyPressEvent(self, event):
-        # keyPressEvent defined in child
-        print('pressed from myDialog: ', event.key())
+        self.setWindowTitle("Iguana")
+        self.resize(760,600)
+        self.scaleFactor = 1
+        self.shiftDown = False
+        self.tabCount= 2
 
+        # create widgets
+        self.button = QPushButton()
+        self.button.setText("one")
+        self.button.clicked.connect(self.runIguana)
+        self.button2 = QPushButton()
+        self.button2.setText("two")
+        self.button3 = QPushButton()
+        self.button3.setText("three")
+        self.button4 = QPushButton()
+        self.button4.setText("four")
 
-class Ui_MainWindow(QWidget):
-    """
-    class Ui_MainWindow
-    """
-    def __init__(self):
-        self.label = ImageLabel()
+        self.imageLabel = QLabel()
+        self.imageLabel.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
+        self.imageLabel.setScaledContents(True)
 
-    def setupUi(self, MainWindow):
-        """ """
-        if MainWindow.objectName():
-            MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(759, 614)
-        self.centralwidget = QWidget(MainWindow)
-        self.centralwidget.setObjectName(u"centralwidget")
-        self.centralwidget.setMinimumSize(QSize(480, 360))
-        self.gridLayout = QGridLayout(self.centralwidget)
-        self.gridLayout.setObjectName(u"gridLayout")
-        self.textBrowser = QTextBrowser(self.centralwidget)
-        self.textBrowser.setObjectName(u"textBrowser")
-        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.textBrowser.sizePolicy().hasHeightForWidth())
-        self.textBrowser.setSizePolicy(sizePolicy)
+        self.scrollArea = QScrollArea()
+        self.scrollArea.setMinimumSize(300,200)
+        self.scrollArea.setWidget(self.imageLabel)
+        self.scrollArea.setVisible(True)
+
+        self.tabWidget = QTabWidget()
+        self.tab1 = QWidget()
+        self.tab2 = QWidget()
+
+        self.textBrowser = QTextEdit()
         self.textBrowser.setMinimumSize(QSize(0, 50))
-        self.textBrowser.setMaximumSize(QSize(16777215, 200))
+        self.textBrowser.setMaximumSize(QSize(16777215, 150))
 
-        self.gridLayout.addWidget(self.textBrowser, 2, 0, 1, 1)
+        # create layouts and add widgets to them
+        self.tab2Layout = QHBoxLayout()
+        self.tab2Layout.addWidget(self.scrollArea)
+        self.tab2.setLayout(self.tab2Layout)
+        self.tabWidget.addTab(self.tab1, "SimulationSetup")
+        self.tabWidget.addTab(self.tab2, "ImageViewer")
 
-        self.horizontalLayout = QHBoxLayout()
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.verticalLayout_2 = QVBoxLayout()
-        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-        self.pushButton_2 = QPushButton(self.centralwidget)
-        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.topLeftLayout = QVBoxLayout()
+        self.topLeftLayout.addWidget(self.button, 0)
+        self.topLeftLayout.addWidget(self.button2, 1)
+        self.topLeftLayout.addWidget(self.button3, 2)
+        self.topLeftLayout.addWidget(self.button4, 3)
 
-        self.verticalLayout_2.addWidget(self.pushButton_2)
+        self.topLayout = QHBoxLayout()
+        self.topLayout.addLayout(self.topLeftLayout)
+        self.topLayout.addWidget(self.tabWidget)
 
-        self.pushButton_3 = QPushButton(self.centralwidget)
-        self.pushButton_3.setObjectName(u"pushButton_3")
-
-        self.verticalLayout_2.addWidget(self.pushButton_3)
-
-        self.pushButton = QPushButton(self.centralwidget)
-        self.pushButton.setObjectName(u"pushButton")
-
-        self.verticalLayout_2.addWidget(self.pushButton)
-
-        self.horizontalLayout.addLayout(self.verticalLayout_2)
-
-        self.tabWidget = QTabWidget(self.centralwidget)
-        self.tabWidget.setObjectName(u"tabWidget")
-        self.tab = QWidget()
-        self.tab.setObjectName(u"tab")
-        self.verticalLayout_3 = QVBoxLayout(self.tab)
-        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
-        self.horizontalLayout_2 = QHBoxLayout()
-        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-
-        self.horizontalLayout_2.addWidget(self.label)
-        self.verticalLayout_3.addLayout(self.horizontalLayout_2)
-
-        self.tabWidget.addTab(self.tab, "")
-        self.tab_2 = QWidget()
-        self.tab_2.setObjectName(u"tab_2")
-        self.tabWidget.addTab(self.tab_2, "")
-
-        self.horizontalLayout.addWidget(self.tabWidget)
-
-        self.horizontalLayout.setStretch(0, 1)
-        self.horizontalLayout.setStretch(1, 3)
-
-        self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 2, 1)
-
-        self.gridLayout.setRowStretch(0, 3)
-        self.gridLayout.setRowStretch(1, 1)
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QMenuBar(MainWindow)
-        self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 759, 21))
-        self.menuFile = QMenu(self.menubar)
-        self.menuFile.setObjectName(u"menuFile")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QStatusBar(MainWindow)
-        self.statusbar.setObjectName(u"statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-
-        self.scaleFactor = 1.0
+        self.mainLayout = QGridLayout()
+        self.mainLayout.addWidget(self.textBrowser, 2, 0, 1, 1)
+        self.mainLayout.addLayout(self.topLayout, 0, 0, 2, 1)
+        # something
         self.createActions()
         self.createMenus()
+        # to see anything you have to make one main widget which you add your main layout to which is then set as the
+        # centralWidget of the mainWindow
+        self.mainWidget = QWidget()
+        self.mainWidget.setLayout(self.mainLayout)
+        self.setCentralWidget(self.mainWidget)
 
-        self.retranslateUi(MainWindow)
+    def keyPressEvent(self, event2):
+        if event2.key() == Qt.Key_Shift:
+            self.shiftDown = True
+            self.scrollArea.verticalScrollBar().setEnabled(False)
 
-        self.tabWidget.setCurrentIndex(0)
-        QMetaObject.connectSlotsByName(MainWindow)
-    # setupUi
+    def keyReleaseEvent(self, event3):
+        if event3.key() == Qt.Key_Shift:
+            self.shiftDown = False
+            self.scrollArea.verticalScrollBar().setEnabled(True)
 
-    def keyPressEvent(self, event):
-        print("hi")
+    def wheelEvent(self, event):
+        # if the shift is held down then when the wheel event triggers, zoom is increased/decreased
+        if self.shiftDown:
+            mouseInput = event.angleDelta().y()
+            if mouseInput != abs(mouseInput):
+                self.scaleFactor *= 0.8
+            else:
+                self.scaleFactor *= 1.25
+            print(self.scaleFactor)
+
+            self.imageLabel.resize(self.scaleFactor * self.imageLabel.pixmap().size())
 
     def createActions(self):
-        self.openAct = QAction("&Open...", None, shortcut="Ctrl+O", triggered=self.loadImage)
+        self.openImageAct = QAction("&Image Tab", self, shortcut="Ctrl+O", triggered=self.loadImageAct)
+        self.choosePathsAct = QAction("&Simulation Paths...", self, shortcut="Ctrl+1", triggered=self.chooseSimulationPaths)
 
     def createMenus(self):
-        self.menubar.addAction(self.menuFile.menuAction())
-        self.menuFile.addAction(self.openAct)
+        self.fileMenu = QMenu("&File", self)
+        self.fileMenu.addAction(self.choosePathsAct)
 
-    def loadImage(self):
-        options = QFileDialog.Options()
-        # fileName = QFileDialog.getOpenFileName(self, "Open File", QDir.currentPath())
-        fileName, _ = QFileDialog.getOpenFileName(None, 'QFileDialog.getOpenFileName()', '',
-                                                  'Images (*.png *.jpeg *.jpg *.bmp *.gif)', options=options)
+        self.newMenu = QMenu('&New', self)
+        self.newMenu.addAction(self.openImageAct)
+        self.newMenu.setEnabled(False)
+
+        self.menuBar().addMenu(self.fileMenu)
+        self.menuBar().addMenu(self.newMenu)
+
+    def chooseSimulationPaths(self):
+        self.simulationPathWindow = SystemPathWindow(self)
+        self.simulationPathWindow.show()
+
+    def loadImageAct(self):
+        self.loadImage(self.getImagePath())
+        self.newImageTab()
+
+    def loadImage(self, fileName):
         if fileName:
             image = QImage(fileName)
+            self.imageLabel = QLabel()
+            self.imageLabel.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
+            self.imageLabel.setScaledContents(True)
+            self.imageLabel.setPixmap(QPixmap.fromImage(image))
+            self.imageLabel.adjustSize()
             if image.isNull():
-                QMessageBox.information(None, "Image Viewer", "Cannot load %s." % fileName)
-                return
+                self.textBrowser.append("Image filepath invalid")
 
-            self.label.setPixmap(QPixmap.fromImage(image))
-            print("hi")
+    def newImageTab(self):
+        self.scrollArea = QScrollArea()
+        self.scrollArea.setMinimumSize(300, 200)
+        self.scrollArea.setWidget(self.imageLabel)
+        self.scrollArea.setVisible(True)
+        newTab = QWidget()
+        newTabLayout = QHBoxLayout()
+        newTabLayout.addWidget(self.scrollArea)
+        newTab.setLayout(newTabLayout)
+        self.tabCount += 1
+        self.tabWidget.addTab(newTab, "Image Tab " + str(self.tabCount))
 
-    def zoomIn(self):
-        self.scaleImage(1.25)
-
-    def zoomOut(self):
-        self.scaleImage(0.8)
-
-    def scaleImage(self, factor):
-        self.scaleFactor *= factor
-        self.label.resize(self.scaleFactor * self.label.pixmap().size())
-
-    def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
-        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
-        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("MainWindow", u"Tab 1", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("MainWindow", u"Tab 2", None))
-        self.menuFile.setTitle(QCoreApplication.translate("MainWindow", u"File", None))
-    # retranslateUi
+    def getImagePath(self):
+        options = QFileDialog.Options()
+        fileName, _ = QFileDialog.getOpenFileName(self, 'QFileDialog.getOpenFileName()', '',
+                                                  'Images (*.png *.jpeg *.jpg *.bmp *.gif)', options=options)
+        return fileName
 
 
-def createGUI():
-    """ """
-    app = QApplication(sys.argv)
-    MainWindow = QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
-    print("hi")
+    def runIguana(self):
+        self.textBrowser.append("Iguana running!")
+        # this will be where iguana will be run from
+        self.getLog()
+        self.imageLabel.setPixmap(QPixmap.fromImage(QImage('chicken.jpg')))
+        self.imageLabel.adjustSize()
+        self.newMenu.setEnabled(True)
+
+    def getLog(self):
+        path = 'C:\\Users\\Philip\\Desktop\\Iguana\\PycharmProjects\\Iguana\\log.log'
+        text = open(path).read()
+        self.textBrowser.append(text)
+
+class SystemPathWindow(QWidget):
+    """
+    This creates a window allowing the user select paths for the simulation paths
+    """
+    def __init__(self, mainWindow):
+        super().__init__()
+        self.resize(500,150)
+        mainLayout = QGridLayout()
+
+        openIcon = self.style().standardIcon(getattr(QStyle,'SP_DirLinkIcon'))
+        self.modellingButton = QPushButton()
+        self.modellingButton.setIcon(openIcon)
+        self.blenderButton = QPushButton()
+        self.blenderButton.setIcon(openIcon)
+        self.loggingButton = QPushButton()
+        self.loggingButton.setIcon(openIcon)
+
+        self.modellingTextBox = QTextEdit()
+        self.modellingTextBox.setPlaceholderText("This should be the path to Iguana/Modelling")
+        self.modellingTextBox.setText("C:\\Users\\Philip\\Desktop\\Iguana\\Modelling")
+        self.modellingTextBox.setMaximumHeight(25)
+        self.blenderTextBox = QTextEdit()
+        self.blenderTextBox.setText("C:\\Program Files\\Blender Foundation\\Blender 2.90\\blender.exe")
+        self.blenderTextBox.setPlaceholderText("This should be the path to blender.exe")
+        self.blenderTextBox.setMaximumHeight(25)
+        self.loggingTextBox = QTextEdit()
+        self.loggingTextBox.setText("C:\\Users\\Philip\\Desktop\\log.log")
+        self.loggingTextBox.setPlaceholderText("This should be the path to where Iguana will place the output log")
+        self.loggingTextBox.setMaximumHeight(25)
+
+        self.saveButton = QPushButton()
+        self.saveButton.setText("Save")
+
+        self.modellingButton.clicked.connect(lambda: self.getGenericPath('modellingButton'))
+        self.blenderButton.clicked.connect(lambda: self.getGenericPath('blenderButton'))
+        self.loggingButton.clicked.connect(lambda: self.getGenericPath('loggingButton'))
+        self.saveButton.clicked.connect(lambda: self.checkValidPaths(mainWindow))
+
+        mainLayout.addWidget(self.modellingButton,0,0)
+        mainLayout.addWidget(self.blenderButton,1,0)
+        mainLayout.addWidget(self.loggingButton,2,0)
+        mainLayout.addWidget(self.saveButton,3,0)
+
+        mainLayout.addWidget(self.modellingTextBox,0,1)
+        mainLayout.addWidget(self.blenderTextBox,1,1)
+        mainLayout.addWidget(self.loggingTextBox,2,1)
+
+        self.setLayout(mainLayout)
+
+    def getGenericPath(self, name):
+        options = QFileDialog.Options()
+        fileName, _ = QFileDialog.getOpenFileName(self, 'QFileDialog.getOpenFileName()', '',
+                                                  '', options=options)
+        if name == 'modellingButton':
+             self.modellingTextBox.setText(fileName)
+        if name == 'blenderButton':
+             self.blenderTextBox.setText(fileName)
+        if name == 'loggingButton':
+             self.loggingTextBox.setText(fileName)
+
+    def checkValidPaths(self, mainWindow):
+        self.close()
+        mainWindow.textBrowser.append("Save Successful")
+        #this will somehow check if the blender and Iguana paths are valid
 
 
--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-from Gui.gui import Ui_MainWindow
+from Gui.gui import MainWindow
 from PyQt5.QtWidgets import QApplication
-from PySide2.QtWidgets import QMainWindow
 import sys
 
 def main():
     app = QApplication(sys.argv)
-    MainWindow = QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    ui = MainWindow()
+    ui.show()
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
     main()
-    print("hi")
